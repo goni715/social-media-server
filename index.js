867f76c690d18c58f268015e8c93528dfd2b6499
goni715 <halesgoni715149@gmail.com>
2024-02-18 17:12:03 +0600

    socket-set-up

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
 const app = require('./app');
+const http = require('http');
+const socketIo = require('socket.io')
+const server = http.createServer(app);
 const PORT=5000;
 
-app.listen(PORT,function () {
+const io = socketIo(server,{
+    cors: {
+        origin: '*',
+        credentials: true
+    }
+})
+
+
+io.on("connection", (socket)=>{
+    console.log('a user connected');
+
+
+    //send-data
+    io.emit("receive-message-from-server", "Hello this is goni");
+
+
+    //receive-message
+    socket.on("send-message-from-client", (data)=>{
+        console.log(data);
+    })
+
+
+    //confirm-request
+    socket.on("confirm-request", (data)=>{
+        if(data){
+            console.log(data);
+            io.emit("success-request", "YES")
+        }
+
+    })
+
+
+    socket.on('disconnect', () => {
+        console.log('user disconnect')
+    })
+
+})
+
+
+server.listen(PORT,function () {
     console.log("Server run @5000")
 })
